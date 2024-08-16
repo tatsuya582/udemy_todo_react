29f55d370f2d4879d6ba6971fef774eb6b221bb2
tatsuya582 <y.k.tsutaya.com_0313@icloud.com>
2024-08-16 18:39:06 +0900

    fix: コンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { InputTodo } from './components/inputTodo';
+import { IncompleteTodos } from './components/incompleteTodos';
+import { CompleteTodos } from './components/completeTodos';
 
 function App() {
   const [todoText, setTodoText] = useState("");
@@ -36,37 +39,20 @@ function App() {
   };
   return (
     <>
-      <div className='input-area'>
-        <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}/>
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-      <div className='incomplete-area'>
-        <p className='title'>未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) =>  (
-              <li key={todo} >
-                <div className='list-row'>
-                  <p className='todo-item'>{todo}</p>
-                  <button onClick={() => onClickComplete(index)}>完了</button>
-                  <button onClick={() => onClickDelete(index)}>削除</button>
-                </div>
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className='complete-area'>
-        <p className='title'>完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => (
-            <li key={todo}>
-              <div className='list-row'>
-                <p className='todo-item'>{todo}</p>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <InputTodo 
+        todoText={todoText} 
+        onChange={onChangeTodoText} 
+        onClick={onClickAdd} 
+      />
+      <IncompleteTodos
+        todos={incompleteTodos} 
+        onClickComplete={onClickComplete} 
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos
+        todos={completeTodos} 
+        onClick={onClickBack}
+      />
     </>
     
   )
