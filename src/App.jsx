a3fbd3c020bc8970cae569e64d62635a96d3d0a0
tatsuya582 <y.k.tsutaya.com_0313@icloud.com>
2024-08-16 17:28:32 +0900

    add: マークアップ

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,48 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='input-area'>
+        <input placeholder='TODOを入力' />
+        <button>追加</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+        <ul>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
+    
   )
 }
 
